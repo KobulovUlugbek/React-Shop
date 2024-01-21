c240f8de5fc0029ae8f6731421bbc46eece7ae7d
Ulugbek Kobulov <108155304+KobulovUlugbek@users.noreply.github.com>
2024-01-21 17:07:44 +0100

    Add header and footer components to App.js

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+        Hause Beck &copy; 2024
+    </footer>
+  )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <span className='logo'>Hause Beck</span>
+            <ul className='nav'>
+                <li>Über uns</li>
+                <li>Kontakte</li>
+                <li>Leistungen</li>
+            </ul>
+        </div>
+
+        <div className='presentation'></div>
+
+    </header>
+  )
+}
